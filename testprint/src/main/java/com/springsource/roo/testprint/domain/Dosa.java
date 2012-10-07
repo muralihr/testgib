824e8f216d0066bf9a9980c467b24f7fc84f636a
MuraliHR <muralihr77@gmail.com>
2012-10-07 13:09:24 +0530

    removed unwanted comments

--- testprint/src/main/java/com/springsource/roo/testprint/domain/Dosa.java
+++ testprint/src/main/java/com/springsource/roo/testprint/domain/Dosa.java
@@ -1,36 +1,14 @@
 /*
- * The Bicycle Sharing Project called NAMMA CYCLE is Licensed under the 
+ * The Bicycle Sharing Project called NAMMA CYCLE is Licensed under the
  * Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
- /**
- * 
- * @author Murali H R 
- * @company Gubbi Labs
- * @version Revision: 0.0 $ $Date: 2012-04-06  (Sat, 6 Apr 2012  )
- **//*
- * The Bicycle Sharing Project called NAMMA CYCLE is Licensed under the 
- * Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -38,120 +16,6 @@
  * limitations under the License.
  */
 
-/**
- * 
- * @author Murali H R 
- * @company Gubbi Labs
- * @version Revision: 0.0 $ $Date: 2012-04-06  (Sat, 6 Apr 2012  )
- **//*
- * The Bicycle Sharing Project called NAMMA CYCLE is Licensed under the 
- * Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/**
- * 
- * @author Murali H R 
- * @company Gubbi Labs
- * @version Revision: 0.0 $ $Date: 2012-04-06  (Sat, 6 Apr 2012  )
- **//*
- * The Bicycle Sharing Project called NAMMA CYCLE is Licensed under the 
- * Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/**
- * 
- * @author Murali H R 
- * @company Gubbi Labs
- * @version Revision: 0.0 $ $Date: 2012-04-06  (Sat, 6 Apr 2012  )
- **//*
- * The Bicycle Sharing Project called NAMMA CYCLE is Licensed under the 
- * Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/**
- * 
- * @author Murali H R 
- * @company Gubbi Labs
- * @version Revision: 0.0 $ $Date: 2012-04-06  (Sat, 6 Apr 2012  )
- **//*
- * The Bicycle Sharing Project called NAMMA CYCLE is Licensed under the 
- * Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/**
- * 
- * @author Murali H R 
- * @company Gubbi Labs
- * @version Revision: 0.0 $ $Date: 2012-04-06  (Sat, 6 Apr 2012  )
- **/  /*
- 2    * Licensed to the Apache Software Foundation (ASF) under one or more
- 3    * contributor license agreements.  See the NOTICE file distributed with
- 4    * this work for additional information regarding copyright ownership.
- 5    * The ASF licenses this file to You under the Apache License, Version 2.0
- 6    * (the "License"); you may not use this file except in compliance with
- 7    * the License.  You may obtain a copy of the License at
- 8    * 
- 9    *      http://www.apache.org/licenses/LICENSE-2.0
- 10    * 
- 11    * Unless required by applicable law or agreed to in writing, software
- 12    * distributed under the License is distributed on an "AS IS" BASIS,
- 13    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- 14    * See the License for the specific language governing permissions and
- 15    * limitations under the License.
- 16    *
- */
 package com.springsource.roo.testprint.domain;
 
 
